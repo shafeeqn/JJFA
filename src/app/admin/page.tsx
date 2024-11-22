39d359c32aca7a32b380a10508f54a51d3a39507
shafeeqn <149598035+shafeeqn@users.noreply.github.com>
2024-11-22 09:34:52 +0530

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,13 +4,13 @@ import React from "react";
 function page() {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
-      <img src="/jam.jpg" className="w-90 h-90 object-cover -my-20" />
+      <img src="/jam.jpg" className="w-96 h-96 object-cover -my-20" />
       <br></br>
-      <p className="lg:text-[66px] text-3xl font-black mb-5 text-center">
+      <p className="lg:text-[72px] text-3xl font-black mb-5 text-center">
         Jamia Junior Fest
       </p>
       <p className="lg:text-lg font-black mb-5 text-center">
-Jamia Junior College Arts Fest <br>2024-'25 Zone A </br>    </p>
+Jamia Junior College Arts Fest '25 <br>Zone A </br>    </p>
       <div className="flex gap-2 text-xl font-semibold">
         <Link href={'/programs'}  className="py-1 bg-yellow border-yellow hover:bg-yellower border-2 px-2 rounded-md ">
           Programs
